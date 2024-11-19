b417edc34b2c87eb4c5ac6c74aaaa39ea99fd9aa
Alon A. Rabinowitz <alonrab@outlook.com>
2024-11-19 16:23:32 +0200

    added a class for the specific OLED

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,14 @@ U8g2Spiral u8g2Spiral = U8g2Spiral();
 
 void setup(void)
 {
-  // u8g2Spiral.begin();
   u8g2Spiral.begin();
+  // u8g2Spiral.start();
 }
 
 void loop(void)
 {
-  u8g2Spiral.DrawSpiral(20);
+  u8g2Spiral.DrawSpiral(10);
+  delay(200);
+  u8g2Spiral.clearBuffer();
+  u8g2Spiral.sendBuffer();
 }
