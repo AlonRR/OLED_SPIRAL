e87cdb43030e70cc71f80a3b5e6e06bdc77f5707
Alon A. Rabinowitz <45129051+AlonRR@users.noreply.github.com>
2024-11-25 19:46:09 +0200

    added default font initialization

--- src/u8g2Spiral.cpp
+++ src/u8g2Spiral.cpp
@@ -2,13 +2,21 @@
 #define HEIGHT 64
 #define WIDTH 128
 
-U8g2Spiral::U8g2Spiral() : U8G2_SSD1315_128X64_NONAME_F_HW_I2C(U8G2_R0, /* reset=*/U8X8_PIN_NONE) {}
+U8g2Spiral::U8g2Spiral() : U8G2_SSD1315_128X64_NONAME_F_HW_I2C(U8G2_R0, /* reset=*/U8X8_PIN_NONE)
+{
+    setFont(u8g2_font_6x10_tf);
+    setFontRefHeightExtendedText();
+    setDrawColor(1);
+    setFontPosTop();
+    setFontDirection(0);
+}
 
 void U8g2Spiral::DrawSpiral(int8_t cycles)
 {
     int x = WIDTH / 2;
     int y = HEIGHT / 2;
     int length = 1;
+    drawStr(0, 0, "Spiral");
     for (int8_t i = 0; i < cycles; i++)
     {
         for (int8_t j = 0; j < 4; j++)
